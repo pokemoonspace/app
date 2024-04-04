3a31689ba4a9993143533f45fa0804e6b8e02c3f
pokemoonspace <161277225+pokemoonspace@users.noreply.github.com>
2024-04-04 09:17:22 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,18 @@ const nextConfig = {
     webpack: config => {
       config.externals.push('pino-pretty', 'lokijs', 'encoding')
       return config
-    }
+    },
+    images: {
+      dangerouslyAllowSVG: true,
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'raw.githubusercontent.com',
+        },
+      ],
+    },
   }
 
 export default nextConfig;
